79434329b0dcd7b71fde39f5ca0c22254b9ae706
Ángel De Ávila <62043490+mr-ang3l@users.noreply.github.com>
2023-10-25 17:40:10 +0000

    file 8.ls advanced finished - directory reader

--- 8.ls-advanced.js
+++ 8.ls-advanced.js
@@ -1,22 +1,50 @@
 const fs = require('node:fs/promises');
+const path = require('node:path');
 
 const folder = process.argv[2] ?? '.';
 
-async function ls (directory) {
-  let files
+async function ls (folder) {
+  let files;
+
   try {
-    files = fs.readdir(folder);
+    files = await fs.readdir(folder);
   } catch {
-    console.erro(`No se pudo leer el directorio ${folder}`);
+    console.error(`No se pudo leer el directorio ${folder}`);
     process.exit(1);
   }
 
-  const filePromise = files.map(async file => {
+  const filesPromises = files.map(async file => {
+
     const filePath = path.join(folder, file);
+    let stats;
+
     try {
-      const fileStats = await fs.stat(filePath); // Status: representa información del archivo
-    } 
+
+      // Status: representa información del archivo
+      stats = await fs.stat(filePath);
+
+    } catch {
+      console.error(`No se pudo leer el directorio ${folder} :(`);
+
+      // Siempre debemos manejar los errores con process.exit(1). El process.exit(0) significa que no hubo errores.
+
+      process.exit(1);
+    }
+
+    // Si pasamos hasta este nivel del bloque de código, quiere decir que todo ha ido bien hasta ahora.
+
+    const isDirectory = stats.isDirectory();
+    const fileType = isDirectory ? 'D-' : 'F-';
+    const fileSize = stats.size.toString();
+    const fileModified = stats.mtime.toLocaleString();
+
+    return `${fileType.padEnd(5)} ${file.padEnd(35)} ${fileSize.padStart(10).padEnd(15)} ${fileModified}`;
   })
+  
+  const filesInfo = await Promise.all(filesPromises);
+
+  filesInfo.forEach(fileInfo => console.log(fileInfo));
+
 }
 
 
